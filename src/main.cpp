a3149f5a42f432de48ef57a9e9fade7487b7136d
hbedard <hbedard@audiokinetic.com>
2020-07-15 21:54:09 -0400

    Add libgit2 to cmake and add a very simple example

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
-
+#include "git2.h"
 
 int main()
 {
+    git_libgit2_init();
+
+    const char* path = "F:\\Personal\\git-ui";
+
+    git_repository* repo;
+    if (git_repository_open(&repo, path) != GIT_OK)
+        return 1;
+
+    git_repository_free(repo);
+
+    git_libgit2_shutdown();
+
     return 0;
 }
